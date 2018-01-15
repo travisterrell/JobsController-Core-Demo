1f11c6b1a1986375de82030b19283862b4341c7e
Travis Terrell <travis@travisterrell.com>
2018-01-15 17:02:53 -0600

    Updated NuGet packages

--- UE2/Program.cs
+++ UE2/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
 
 namespace UE2
 {
